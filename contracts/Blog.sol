8d48817fb16c995f9bbdeb097466b91386bbc631
Jorge Zerpa <jorgezerpacoder@gmail.com>
2024-10-31 18:06:52 -0400

    [feature] Blog contract -> add events to create, update and likes related methods

--- contracts/Blog.sol
+++ contracts/Blog.sol
@@ -15,11 +15,16 @@ contract Blog {
     
     Post[] public posts;
     mapping(address => Post[]) public userPosts;
+
+    event PostCreated(uint256 indexed postId, address indexed author);
+    event PostUpdated(uint256 indexed postId, address indexed author);
+    event PostLikedDisliked(uint256 indexed postId, address indexed user, bool isLike);
     
     function createPost(string memory _title, string memory _content) public {
         Post memory newPost = Post(posts.length, _title, _content, msg.sender, block.timestamp, 0, 0, false);
         posts.push(newPost);
         userPosts[msg.sender].push(newPost);
+        emit PostCreated(newPost.id, msg.sender);
     }
     
 	function getPost(uint256 _postId) public view returns (Post memory) {
@@ -34,13 +39,16 @@ contract Blog {
         require(msg.sender == posts[_postId].author, "Only the author can update the post");
         posts[_postId].title = _newTitle;
         posts[_postId].content = _newContent;
+        emit PostUpdated(_postId, msg.sender);
     }
 
-		function likePost(uint256 _postId) public {
+	function likePost(uint256 _postId) public {
         posts[_postId].likes++;
+        emit PostLikedDisliked(_postId, msg.sender, true);
     }
 
     function dislikePost(uint256 _postId) public {
         posts[_postId].dislikes++;
+        emit PostLikedDisliked(_postId, msg.sender, false);
     }
 }
